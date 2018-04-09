c5d4badcf1c2ffbbefc94d404670bee82096dfb6
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 19:44:23 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -1,4 +1,6 @@
 from connection import Connection
 
 conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
-conn.ssh_remote(['ls'])
+print(conn.ssh_remote(['ls', 'pwd']))
+
+
