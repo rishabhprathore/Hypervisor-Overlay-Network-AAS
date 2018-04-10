4eecea60bebf7af33f043489549d59adf8042c27
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 23:17:38 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -1,16 +1,16 @@
 from connection import Connection
 import functions
 #conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
-#functions.get_connection()
-#functions.create_namespace('testNS2', primary=True)
-#functions.create_vethpair('test_veth0','test_veth1',primary=True)
-#functions.set_link_up('test_veth0')
-#functions.set_link_up('test_veth1')
-#functions.move_veth_to_namespace('test_veth0', 'testNS2', primary=True)
-#functions.assign_ip_address_namespace('testNS2', 'test_veth0', '99.99.99.1/24', primary=True)
-#functions.set_link_up_in_namespace('testNS2', 'test_veth0', primary=True)
-#functions.assign_ip_address('test_veth1', '99.99.99.2/24', primary=True)
-functions.set_link_up('test_veth1', primary=True)
+functions.get_connection()
+functions.create_namespace('testNS3', primary=True)
+functions.create_vethpair('test_veth2','test_veth3',primary=True)
+functions.set_link_up('test_veth2')
+functions.set_link_up('test_veth3')
+functions.move_veth_to_namespace('test_veth3', 'testNS3', primary=True)
+functions.assign_ip_address_namespace('testNS3', 'test_veth3', '99.99.98.1/24', primary=True)
+functions.set_link_up_in_namespace('testNS3', 'test_veth3', primary=True)
+functions.assign_ip_address('test_veth2', '99.99.98.2/24', primary=True)
+functions.set_link_up('test_veth2', primary=True)
 print ''
 
 
