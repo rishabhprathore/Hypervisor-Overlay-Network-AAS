eaa843f47429f0e0abdde08a15f7d06932006dcb
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 20:17:11 -0400

    test

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-from connections import Connection
+from connection import Connection
 import os
 
 conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
 prefix = 'sudo ip netns exec '
 
-def create_namespace(name, primary):
+def create_namespace(name, primary='True'):
     cmd = 'sudo ip netns add {}'.format(name)
     if primary==True:
         os.system(cmd)
