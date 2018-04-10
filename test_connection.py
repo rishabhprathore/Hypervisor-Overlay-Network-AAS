2cf7886fc6a98d67b9bb544e45e918c3e81e4056
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 23:07:48 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -1,5 +1,21 @@
 from connection import Connection
 import functions
 #conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
-functions.create_namespace('testNS1')
+fucntions.get_connection()
+functions.create_namespace('testNS2', primary=True)
+functions.create_vethpair('test_veth0','test_veth1',primary=True)
+funtions.set_link_up('test_veth0')
+funtions.set_link_up('test_veth1')
+functions.move_veth_to_namespace('test_veth0', 'testNS2', primary=True)
+functions.assign_ip_address_namespace('testNS2', 'test_veth0', '99.99.99.1/24', primary=True)
+functions.set_link_up_in_namespace('testNS2', 'test_veth0', primary=True)
+functions.assign_ip_address('test_veth1', '99.99.99.2/24', primary=True)
+functions.set_link_up_in('test_veth1', primary=True)
+
+
+
+
+
+
+
 
