6f1c98ed9b5b8c3f17ac43281bd2c34ada766fb8
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 21:31:23 -0400

    test

---
+++ data.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+data_store={'PGW-T1':{'NS1':{'interfaces':{'veth1':'192.168.1.1',
+'veth3':'192.168.3.1'},
+},'NS2':{'interfaces':{'veth2':'192.168.2.1',
+'veth4':'192.168.4.1'},
+}}
+}
+
+pprint(data_store)
