bc11d77e5a2a30c193819ca4862d7260f8150d32
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 23:12:24 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -4,13 +4,13 @@ import functions
 #functions.get_connection()
 #functions.create_namespace('testNS2', primary=True)
 #functions.create_vethpair('test_veth0','test_veth1',primary=True)
-functions.set_link_up('test_veth0')
-functions.set_link_up('test_veth1')
-functions.move_veth_to_namespace('test_veth0', 'testNS2', primary=True)
-functions.assign_ip_address_namespace('testNS2', 'test_veth0', '99.99.99.1/24', primary=True)
-functions.set_link_up_in_namespace('testNS2', 'test_veth0', primary=True)
-functions.assign_ip_address('test_veth1', '99.99.99.2/24', primary=True)
-functions.set_link_up_in('test_veth1', primary=True)
+#functions.set_link_up('test_veth0')
+#functions.set_link_up('test_veth1')
+#functions.move_veth_to_namespace('test_veth0', 'testNS2', primary=True)
+#functions.assign_ip_address_namespace('testNS2', 'test_veth0', '99.99.99.1/24', primary=True)
+#functions.set_link_up_in_namespace('testNS2', 'test_veth0', primary=True)
+#functions.assign_ip_address('test_veth1', '99.99.99.2/24', primary=True)
+functions.set_link_up('test_veth1', primary=True)
 
 
 
