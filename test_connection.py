99c2bbd95e1799e146372735c07e5eb15ee2ea13
rishabhprathore <rrathor@ncsu.edu>
2018-04-09 20:25:20 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -1,5 +1,5 @@
 from connection import Connection
 import functions
-#conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
-functions.create_namespace(testNS1)
+conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
+functions.create_namespace(conn, 'testNS1')
 
