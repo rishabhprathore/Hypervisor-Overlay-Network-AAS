dc5ecf10723b6dd74c7b28845c8bf79faf6c58c6
Surbhi Bansal <surbhibansal@Surbhis-MacBook-Air.local>
2018-04-09 22:01:24 -0400

    module1

--- functions.py
+++ functions.py
@@ -50,7 +50,7 @@ def set_link_up_in_namespace(name_space, interface, primary=True):
 
 def assign_ip_address_namespace(name_space, interface, ip_address, primary=True):
     global prefix
-    cmd = prefix + name_space + 'ip addr add '+ ip_address + 'dev {}'.format(interface)
+    cmd = prefix + name_space + 'ip addr add '+ ip_address + ' dev {}'.format(interface)
     set_link_up_in_namespace(name_space, interface, primary)
     if primary==True:
         os.system(cmd)
@@ -66,3 +66,23 @@ def assign_ip_address(interface, ip_address, primary=True):
         return
     conn.ssh_remote([cmd])
     return
+
+
+def move_veth_to_namespace(vethname, name_space, primary=True):
+    cmd = 'sudo ip link set {} netns {}'.format(vethname, name_space)
+    if primary==True:
+        os.system(cmd)
+        return
+    conn.ssh_remote([cmd])
+    return
+
+
+def move_veth_to_bridge(vethname, bridge_name, primary=True):
+    cmd = 'sudo brctl addif {} {}'.format(bridge_name, vethname)
+    if primary==True:
+        os.system(cmd)
+        return
+    conn.ssh_remote([cmd])
+    return
+
+
