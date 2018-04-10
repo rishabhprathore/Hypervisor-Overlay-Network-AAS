86034187a55f5505cbeb1f4b27fcac17dc485c5d
rishabhprathore <rrathor@ncsu.edu>
2018-04-10 01:45:46 -0400

    test

--- test_connection.py
+++ test_connection.py
@@ -2,6 +2,12 @@ from connection import Connection
 import functions
 #conn = Connection(remote_ip='152.46.18.27', username='ckogant', pkey_path='/root/.ssh/id_rsa')
 functions.get_connection()
+functions.create_gre_tunnel('1.1.1.1', '2.2.2.2', 'gre_test', primary=True):
+
+
+print ''
+
+"""
 functions.create_namespace('testNS3', primary=True)
 functions.create_vethpair('test_veth2','test_veth3',primary=True)
 functions.set_link_up('test_veth2')
@@ -11,7 +17,7 @@ functions.assign_ip_address_namespace('testNS3', 'test_veth3', '99.99.98.1/24', 
 functions.set_link_up_in_namespace('testNS3', 'test_veth3', primary=True)
 functions.assign_ip_address('test_veth2', '99.99.98.2/24', primary=True)
 functions.set_link_up('test_veth2', primary=True)
-print ''
+"""
 
 
 
