e3cbae50b0ccf54cf72bdcedf7f88d9e2e147d2f
Verma <sunny.verma@nokia.com>
2018-04-15 01:51:12 -0400

    test

---
+++ bg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true; do
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+sleep 3
+done
