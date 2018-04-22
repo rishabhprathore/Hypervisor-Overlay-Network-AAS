69818de37ab97e4e8edb2416a9a13593e4187245
Aasheesh Tandon <atandon@ncsu.edu>
2018-04-21 20:34:05 -0400

    Integrated Docker EnvSetup in envSetup

--- envSetup.sh
+++ envSetup.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-echo -e "\n## NOTE ## ---> ** Before running the script, switch to root user! If you are not root, "CTRL + C" now!!! **"
-sleep 3
+#echo -e "\n## NOTE ## ---> ** Before running the script, switch to root user! If you are not root, "CTRL + C" now!!! **"
+#sleep 3
 echo -e "\n\n******Starting with environment setup******\n"
-apt-get -y install qemu-kvm
+sudo apt-get -y install qemu-kvm
 sleep 2
-apt-get -y install libvirt-bin
+sudo apt-get -y install libvirt-bin
 sleep 2
-apt-get -y install virt-manager
+sudo apt-get -y install virt-manager
 sleep 2
-apt-get -y install virt-viewer
+sudo apt-get -y install virt-viewer
 sleep 2
-apt-get -y install libguestfs-tools
+sudo apt-get -y install libguestfs-tools
 sleep 2
-apt-get install python-libvirt
+sudo apt-get install python-libvirt
 sleep 2
-apt-get install libvirt-doc
+sudo apt-get install libvirt-doc
 sleep 2
-apt install -y python-pip
+sudo apt install -y python-pip
 sleep 2
-pip install --upgrade pip
+sudo pip install --upgrade pip
 sleep 2
-apt-get install python-paramiko -y
+sudo apt-get install python-paramiko -y
 sleep 2
 #sudo python -m easy_install --upgrade pyOpenSSL
-apt-get install python-jinja2 -y
+sudo apt-get install python-jinja2 -y
 sleep 2
-apt-get install python-yaml -y
+sudo apt-get install python-yaml -y
 sleep 2
-wget http://distro.ibiblio.org/tinycorelinux/9.x/x86/release/TinyCore-current.iso -O /root/TinyCore.iso
+sudo wget http://distro.ibiblio.org/tinycorelinux/9.x/x86/release/TinyCore-current.iso -O /root/TinyCore.iso
 
 sudo sed -i -e 's/#user/user/g' /etc/libvirt/qemu.conf
 sudo sed -i -e 's/#group/group/g' /etc/libvirt/qemu.conf
