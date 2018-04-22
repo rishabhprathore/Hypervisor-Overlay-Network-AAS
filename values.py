16e4d16d9ca3a8bd84232ab594fe7a31d74560ab
Aasheesh Tandon <atandon@ncsu.edu>
2018-04-21 23:37:45 -0400

    added user lever json

--- values.py
+++ values.py
@@ -1,3 +1,5 @@
+import json
+
 def get_value():
     primary_data = {
         'username': 'rirathor',
@@ -16,75 +18,79 @@ def get_value():
     }
     return primary_data, secondary_data, tertiary_data
 def get_user_data():
-    user_data = {
-        'data': {
-            'tenants': [{
-                'id': "3",
-                'primary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.3"],
-                    }, {
-                        "cidr": "1.5.5.0/24",
-                        "vm_ips": ["1.5.5.2"],
-                    }]
-                },
-                'secondary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.5"],
-                    }, {
-                        "cidr": "1.6.6.0/24",
-                        "vm_ips": ["1.6.6.2"],
-                    }
-
-                    ]
-                },
-                'tertiary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.5"],
-                    }, {
-                        "cidr": "1.6.6.0/24",
-                        "vm_ips": ["1.6.6.2"],
-                    }
-
-                    ]
-                }
-
-            },
-                {
-                'id': "4",
-                'primary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.3"],
-                    }, {
-                        "cidr": "1.5.5.0/24",
-                        "vm_ips": ["1.5.5.2"],
-                    }]
-                },
-                'secondary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.5"],
-                    }, {
-                        "cidr": "1.6.6.0/24",
-                        "vm_ips": ["1.6.6.2"],
-                    }
-
-                    ]
-                },
-                'tertiary': {
-                    "subnets": [{
-                        "cidr": "1.2.2.0/24",
-                        "vm_ips": ["1.2.2.5"],
-                    }, {
-                        "cidr": "1.6.6.0/24",
-                        "vm_ips": ["1.6.6.2"],
-                    }
-
-                    ]
-                }
-            }]}}
-    return user_data
+    with open("user_data.json", 'r') as f:
+        user_data = json.load(f)
+        #print(user_data)
+        return user_data
+    #
+    # user_data = {
+    #     'data': {
+    #         'tenants': [{
+    #             'id': "3",
+    #             'primary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.3"],
+    #                 }, {
+    #                     "cidr": "1.5.5.0/24",
+    #                     "vm_ips": ["1.5.5.2"],
+    #                 }]
+    #             },
+    #             'secondary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.5"],
+    #                 }, {
+    #                     "cidr": "1.6.6.0/24",
+    #                     "vm_ips": ["1.6.6.2"],
+    #                 }
+    #
+    #                 ]
+    #             },
+    #             'tertiary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.5"],
+    #                 }, {
+    #                     "cidr": "1.6.6.0/24",
+    #                     "vm_ips": ["1.6.6.2"],
+    #                 }
+    #
+    #                 ]
+    #             }
+    #
+    #         },
+    #             {
+    #             'id': "4",
+    #             'primary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.3"],
+    #                 }, {
+    #                     "cidr": "1.5.5.0/24",
+    #                     "vm_ips": ["1.5.5.2"],
+    #                 }]
+    #             },
+    #             'secondary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.5"],
+    #                 }, {
+    #                     "cidr": "1.6.6.0/24",
+    #                     "vm_ips": ["1.6.6.2"],
+    #                 }
+    #
+    #                 ]
+    #             },
+    #             'tertiary': {
+    #                 "subnets": [{
+    #                     "cidr": "1.2.2.0/24",
+    #                     "vm_ips": ["1.2.2.5"],
+    #                 }, {
+    #                     "cidr": "1.6.6.0/24",
+    #                     "vm_ips": ["1.6.6.2"],
+    #                 }
+    #
+    #                 ]
+    #             }
+    #         }]}}
