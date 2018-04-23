5c9d11964bfe9652615775293cf27b36e394bc33
Aasheesh Tandon <atandon@ncsu.edu>
2018-04-23 01:51:32 -0400

    update

--- connection.py
+++ connection.py
@@ -14,10 +14,12 @@ class Connection:
             self.ssh = paramiko.SSHClient()
             privkey = paramiko.RSAKey.from_private_key_file(pkey_path)
             self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.secondary_ssh = self.ssh.connect(secondary_data['ip'], username=secondary_data['username'], pkey=privkey)
-            self.tertiary_ssh = self.ssh.connect(
-                tertiary_data['ip'], username=tertiary_data['username'], pkey=privkey)
+            self.ssh.connect(secondary_data['ip'], username=secondary_data['username'], pkey=privkey)
+            self.secondary_ssh = self.ssh
 
+            self.ssh.connect(
+                tertiary_data['ip'], username=tertiary_data['username'], pkey=privkey)
+            self.tertiary_ssh = self.ssh
             self.primary_conn=libvirt.open('qemu:///system')
             self.secondary_con = libvirt.open(
                 'qemu+ssh://{}@{}/system'.format(secondary_data['username'], secondary_data['ip']))
