6b9b5ba909e2a2f5badd45ea85569a60532e619d
rishabhprathore <rrathor@ncsu.edu>
2018-04-24 21:19:48 -0400

    test

--- test_code.py
+++ test_code.py
@@ -13,7 +13,8 @@ from pprint import pprint
 def main():
     
     primary_data, secondary_data, tertiary_data = values.get_value()
-    conn = Connection(secondary_data, tertiary_data)
+    conn = None
+    #conn = Connection(secondary_data, tertiary_data)
     #for a in values.get_user_data()['data']['tenants']:
     #    tm.run(a, conn)
     tenant = values.get_user_data()['data']['tenants'][0]
