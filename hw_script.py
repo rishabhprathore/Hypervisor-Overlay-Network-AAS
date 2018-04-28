240760600de6fc01511010f84c5c6da18a254ec4
rishabhprathore <rrathor@ncsu.edu>
2018-04-27 20:43:20 -0400

    test

--- hw_script.py
+++ hw_script.py
@@ -26,8 +26,10 @@ docker_con = docker.from_env()
 
 def create_container(c_name):
     host_c = cli.create_host_config(privileged=True)
-    c_id = docker_con.containers.list(filters={'name':c_name})
-    if not c_id:
+    c_id_list = docker_con.containers.list(filters={'name':c_name})
+    if c_id_list:
+        c_id = c_id_list[0].id
+    if not c_id_list:
         c_id = cli.create_container(image='atandon70/ubuntu_project:loadedUBUNTUimage',
                                     command='/bin/sleep 30000000',
                                     host_config=host_c,
